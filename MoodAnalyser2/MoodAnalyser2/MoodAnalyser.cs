e5caa27b7fd63798d27859ac13851a3b7ca82d74
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-28 12:47:16 +0530

    Testing mood

---
+++ MoodAnalyser2/MoodAnalyser2/MoodAnalyser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyser2
+{
+   public class MoodAnalyser
+    {
+        string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyzeMood()
+        {
+            if (message.ToLower().Contains("happy"))
+            {
+                return "happy";
+            }
+            else
+            {
+                return "sad";
+            }
+        }
+    }
+}
